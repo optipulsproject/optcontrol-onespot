f69d8d2a2b5a6a0f989bb2b35368c6e1a061b83b
Roland Herzog <roland.herzog@mathematik.tu-chemnitz.de>
2019-09-09 17:17:12 +0200

    adds submodule initialization to update script

--- bin/numapde-submodules-update.sh
+++ bin/numapde-submodules-update.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 # This script updates all or selected submodules within the current repository.
 
+# Initialize all submodules, in case this has not been done
+git submodule init
+
 # Pull from all submodules 
 git submodule update --remote --merge
 
