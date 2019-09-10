3aa93c6ab0cb550a2e2cf2eb81f5b965bf2a7807
Andreas Naumann <andreas.naumann@mathematik.tu-chemnitz.de>
2019-09-10 21:33:56 +0200

    rewrite the readme in the new repository with the new title and repository paths

--- bin/createNewPublication.py
+++ bin/createNewPublication.py
@@ -4,6 +4,7 @@ import requests
 import json
 import argparse
 import sys
+import time
 
 parser=argparse.ArgumentParser(description='prepare fork for new publication')
 parser.add_argument('login_token', metavar='token', help='the private token for login') #, type=basestring)
@@ -37,9 +38,11 @@ headers={'Private-Token' : privateToken}
 #print(r.text)
 #print(r.status_code)
 
+#fork the template
 templateId=5326
 #sandboxId=2576
-url='https://gitlab.hrz.tu-chemnitz.de/api/v4/projects/%(templateId)d/fork' %{'templateId':templateId}
+urlFormat='https://gitlab.hrz.tu-chemnitz.de/api/v4/projects/%(projectId)d'
+url=urlFormat %{'projectId':templateId}+'/fork'
 #print(url)
 payload={'namespace':'numapde/sandbox','name':pubTitle,'id':templateId, 'path' : pubPath}
 print(payload)
@@ -49,4 +52,23 @@ print(r.text)
 if(r.status_code != 201):
     print('something went wrong during forking. got status code %d and result text %s' % (r.status_code, r.text))
     sys.exit(1)
+
+#prepare the new README.md
+#https://docs.gitlab.com/ee/api/repository_files.html 
 newProject=json.loads(r.text)
+newId=newProject['id']
+#TODO: check if the project is ready after each pause
+print('wait some seconds and let gitlab create the project')
+time.sleep(5)
+url=urlFormat %{'projectId' : newId}
+#TODO: do this url-encoding in the right way..
+url=url+'/repository/files/README%2Emd'
+with open('README.md.in') as file:
+    readme = file.read()
+readme=readme % { 'PUBLICATION_TITLE' : pubTitle, 'http_url_to_repo' : newProject['http_url_to_repo'], 'ssh_url_to_repo' : newProject['ssh_url_to_repo']}
+payload={'file_path':'README%2Emd', 'branch' : 'master', 'content' : readme, 'commit_message' : 'replace the readme with title and project information'}
+rReadme=requests.put(url, headers=headers, data=payload)
+if(rReadme.status_code != 200):
+    print("the readme update did not work, got response: "+rReadme.text)
+    sys.exit(1)
+print('response after readme update: '+rReadme.text)
