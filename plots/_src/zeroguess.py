4d15c5252723503df6aab4a1a0b9c1c692a4461c
Dmytro Strelnikov <d.strelnikov@inventati.org>
2021-08-16 14:10:00 +0200

    plots: fix labels in zeroguess plot

--- plots/_src/zeroguess.py
+++ plots/_src/zeroguess.py
@@ -17,12 +17,10 @@ powers = parameters.zeroguess['powers']
 times =  parameters.zeroguess['times']
 
 
-controls = [
-                [np.load(f'numericals/zeroguess/{P_YAG}-{T:5.3f}.npy')
-                    for T in times
-                ]
-                for P_YAG in powers
-           ]
+# controls = [
+#         np.load(filename)
+#         for filename in parameters.zeroguess['optcontrols']
+#         ]
 
 matplotlib.rc('font', **font)
 
@@ -34,26 +32,22 @@ gs = fig.add_gridspec(3, 3, hspace=0, wspace=0,
 axes = gs.subplots(sharex='col', sharey='row')
 
 
-# for ax_triple, control_triple in zip(axes, controls):
-#     for ax, control in zip(ax_triple, control_triple):
-#         ax.plot(control)
-#         ax.set_ylim(0, 1.1)
-#         ax.yaxis.set_major_formatter(ticker.FuncFormatter(lambda y, _: '{:g}'.format(y)))
-
 for ax_triple, P_YAG in zip(axes, powers):
     for ax, T in zip(ax_triple, times):
         control = np.load(f'numericals/zeroguess/{P_YAG}-{T:5.3f}.npy')
-        ax.plot(control * P_YAG)
-        ax.set_ylim(0, 1.1 * P_YAG)
+        ax.plot(np.linspace(0, T, len(control)), control * P_YAG)
+        ax.set_ylim(0, P_YAG)
+        ax.set_xlim(0, T)
+        if T == min(times):
+            ax.xaxis.set_ticks(np.arange(0, T + 0.0001, 0.005))
+        else:
+            ax.xaxis.set_ticks(np.arange(0.005, T + 0.0001, 0.005))
+        if P_YAG == max(powers):
+            ax.yaxis.set_ticks(np.arange(0, P_YAG + 1, 500))
+        else:
+            ax.yaxis.set_ticks(np.arange(500, P_YAG + 1, 500))
         ax.yaxis.set_major_formatter('{x:4.0f} W')
-        # ax.xaxis.set_major_formatter('{x:4.0f}')
-        # ax.yaxis.set_major_formatter(
-        #     ticker.FuncFormatter(
-        #         lambda y, pos: '{:4.0f}'.format(y * P_YAG)
-        #         )
-        #     )
-# axes[0,0].yaxis.set_minor_locator(ticker.MultipleLocator(3))
-# axes[0,0].yaxis.set_major_formatter('{x:1.1f}')
+        ax.xaxis.set_major_formatter(lambda x, pos: str(int(x*1000)) + ' ms')
 
 plt.tight_layout()
 plt.savefig(args.outfile)
